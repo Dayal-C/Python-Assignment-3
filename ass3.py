687024bcc70f90591946225bea0bdf13c001194e
Dayal-C <145312735+Dayal-C@users.noreply.github.com>
2024-01-09 08:56:40 +0530

    Add files via upload

---
+++ ass3.py
@@ -0,0 +1,28 @@
+class Person:
+    def _init_(self, name, age, address):
+        self.name = name
+        self.age = age
+        self.address = address
+
+    def update_information(self, name=None, age=None, address=None):
+        if name:
+            self.name = name
+        if age:
+            self.age = age
+        if address:
+            self.address = address
+
+    def display_information(self):
+        print("Name:",self.name)
+        print("Age:",self.age)
+        print("Address:",self.address)
+
+person1 = Person(name="Vimal Varghese", age=22, address="248-b vytilla")
+
+print("Initial Information:")
+person1.display_information()
+
+person1.update_information(age=23, address="123-C Cherthala")
+
+print("\nUpdated Information:")
+person1.display_information()
